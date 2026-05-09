2b4bc3825e91417d343387d2d20599aa7d444b16
Dayvid <dayvid.jhonatas@escolar.ifrn.edu.br>
2026-05-09 08:43:54 -0300

    chatsHTML,JSeValidação.js

---
+++ js/chats.js
@@ -0,0 +1,102 @@
+let usuarioAtual =
+localStorage.getItem("usuarioLogado");
+
+document.getElementById("usuarioLogado")
+.innerText = usuarioAtual;
+
+
+const chats = {
+
+    chat1: [],
+
+    chat2: []
+
+};
+
+
+let chatAtual = "chat1";
+
+
+function carregarMensagens() {
+
+    const areaMensagens =
+    document.getElementById("mensagens");
+
+    areaMensagens.innerHTML = "";
+
+    chats[chatAtual].forEach(msg => {
+
+        areaMensagens.innerHTML += `
+
+            <div class="bg-blue-700 p-3 rounded-lg">
+
+                <strong class="text-yellow-300">
+                    ${msg.usuario}
+                </strong>
+
+                <p>
+                    ${msg.texto}
+                </p>
+
+            </div>
+
+        `;
+
+    });
+
+}
+
+
+function trocarChat(chat) {
+
+    chatAtual = chat;
+
+    const titulo =
+    document.getElementById("tituloChat");
+
+    if(chat == "chat1") {
+
+        titulo.innerText = "Joao";
+
+    }
+
+    else {
+
+        titulo.innerText = "Maria";
+
+    }
+
+    carregarMensagens();
+
+}
+
+
+function enviarMensagem() {
+
+    const input =
+    document.getElementById("mensagemInput");
+
+    const texto = input.value;
+
+    if(texto.trim() == "") {
+
+        return;
+
+    }
+
+    chats[chatAtual].push({
+
+        usuario: usuarioAtual,
+
+        texto: texto
+
+    });
+
+    input.value = "";
+
+    carregarMensagens();
+
+}
+
+
+carregarMensagens();
--- js/validacao.js
+++ js/validacao.js
@@ -1,22 +1,51 @@
 const usuarios = ['mimmarcelo', 'Luiran', 'Dayvid', 'Mauro', 'Lucas'];
+
 const senhas = ['Teste123', '123', '123', '123', '123'];
 
 function validacao() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    
+
+    var username =
+    document.getElementById('username').value;
+
+    var password =
+    document.getElementById('password').value;
+
     var contador = 0;
+
     var validador = 0;
+
     while(contador < usuarios.length && validador == 0) {
-        if(username == usuarios[contador] && password == senhas[contador]) {
+
+        if(username == usuarios[contador] &&
+           password == senhas[contador]) {
+
             validador++;
+
+            // SALVA USUÁRIO
+            localStorage.setItem(
+                "usuarioLogado",
+                username
+            );
+
         }
+
         contador++;
+
     }
 
     if(validador == 1) {
-        return alert(`Seja bem-vindo, ` + username);
-    } else {
-        return alert('Desculpe... Esse usuário não existe!');
+
+        alert(`Seja bem-vindo, ` + username);
+
+        // ABRE O CHAT
+        window.location.href = "chats.html";
+
     }
+
+    else {
+
+        alert('Desculpe... Esse usuário não existe!');
+
+    }
+
 }
